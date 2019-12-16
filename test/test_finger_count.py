88e61657707c0dfd8dc3638a975b2f8c5ed7e610
0xleo <0xleo.git@gmail.com>
2019-12-16 17:08:53 +0200

    complete unit test for finger counter

--- test/test_finger_count.py
+++ test/test_finger_count.py
@@ -11,13 +11,27 @@ import glob
 
 class TestKMeans(unittest.TestCase):
     def setUp(self):
-        # create auxilary members
-        self.files_to_read = glob.glob('../test/count_fingers/*.jpg') +\
-            glob.glob('../test/count_fingers/*.png')
+        # create auxiliary members
+        test_path = os.path.join('..', 'test_images', 'count_fingers')
+        files_jpg = os.path.join(test_path, '*.jpg')
+        files_png = os.path.join(test_path, '*.png')
+        file_gt = os.path.join(test_path, 'ground_truth.txt')
+        self.files_to_read = glob.glob(files_jpg) + glob.glob(files_png)
+        assert(len(self.files_to_read) and os.path.isfile(file_gt)),\
+            'Directory %s must contain image1, image2,... and'\
+            'a file ground_truth.txt -> <image_file> <no_fingers>'\
+            % test_path 
+        self.ground_truth = {}
+        with open(file_gt) as f:
+            for l in f.readlines():
+                self.ground_truth[l.strip().split(' ')[0]] =\
+                        int(l.strip().split(' ')[1])
+
 
     def tearDown(self):
-        # delete auxilary members
+        # delete auxiliary members
         pass
+
 
     def read_image(self, fpath):
         '''
@@ -28,15 +42,28 @@ class TestKMeans(unittest.TestCase):
         hsv = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
         return hsv
 
-    def test_create_skin_detector(self):
+
+    def test_finger_counter(self):
         '''
-        Require skin detector to have some hsv_low and hsv_high
-        limits, ideally set to either None or some default value.
+        Counts number of fingers and compares against GT
         '''
         # TODO: count fingers, if good accuracy pass tests
+        results = {}
         for f in self.files_to_read:
             hsv = self.read_image(f)
-        self.assertEqual(True, True)
+            km = KMeans(hsv, k = 3)
+            km.do_kmeans()
+            km.find_most_frequent()
+            # key of self.ground_truth
+            fname = f.split(os.sep)[-1]
+            n = km.count_fingers()
+            results[f] = (n, self.ground_truth[fname] == n)
+        total_correct = len([r[1] for r in results.values() if r[1]])
+        print("%s: %d/%d correct results" %\
+            (self.test_finger_counter.__name__,total_correct, len(results)))
+        # 3 possible classfications.
+        # Perform better than random classifier? Then pass.
+        self.assertGreater(total_correct, len(results)/3)
 
 
 if __name__ == '__main__':
