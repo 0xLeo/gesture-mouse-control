d1bd7421121d62e96883b1efef2f6fa77446f9e8
0xleo <0xleo.git@gmail.com>
2020-01-31 23:20:04 +0100

    Start implementing heat map class

---
+++ src/heatmap.py
@@ -0,0 +1,30 @@
+import sys
+sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
+import cv2
+import numpy as np
+
+class Heatmap():
+    def __init__(self, rows_cols: tuple):
+        self.heatmap = np.zeros((rows_cols[0], rows_cols[1]), np.uint8)
+        self.max_hits = 5
+
+
+    def update(self, x, y, w, h):
+        """
+        faceCascade.detectMultiScale returns (x, y, w, h)
+        Pass (x, y), (x+w, y+h) to this method
+        The region inside that rectangle adds +1 to the map
+
+        """
+        self.heatmap -= 1
+        # fix underflow
+        self.heatmap[self.heatmap == 255] = 0
+        self.heatmap[y:y+h, x:x+w] += 2
+        self.heatmap[self.heatmap > self.max_hits] = self.max_hits
+
+
+    def find_centre(self) -> tuple:
+        """
+        get the centre of the highest area
+        """
+        pass
