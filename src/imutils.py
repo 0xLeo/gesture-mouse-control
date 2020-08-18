3634bb3871c034354432fb47d41c9bed2bd440e9
0xleo <0xleo.git@gmail.com>
2020-08-19 02:00:38 +0300

    Add method to type text on image

--- src/imutils.py
+++ src/imutils.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from typing import Union, List, Tuple
 
 
 class ImUtils:
@@ -16,7 +17,7 @@ class ImUtils:
         Parameters
         ----------
         im : np.ndarray
-            The image (2D e.g. grey 3D e.g. RGB) to show 
+            The image (2D e.g. grey 3D e.g. RGB) to show
         delay : float
             How long to show the image for. It's ignored if a key is assigned to`keypress`
         title :
@@ -27,8 +28,8 @@ class ImUtils:
         """
         if keypress is not None:
             if len(keypress) != 1 or not keypress.isalnum():
-                print("Parameter keypress (=%s) must be a single alphanumeric character" %
-                        str(keypress))
+                print("Parameter keypress (=%s) must be a single"
+                        "alphanumeric character" % str(keypress))
                 return
             keypress = ord(keypress) & 0xff
         key_cur = None
@@ -40,3 +41,54 @@ class ImUtils:
             cv2.imshow(title, im)
             cv2.waitKey(delay=int(delay*1000))
         cv2.destroyWindow(title)
+
+
+    @classmethod
+    def text_on_im(self, im: np.ndarray,
+            text: str,
+            pos: Union[str, Tuple[int, int]] = 'tr',
+            font: str = cv2.FONT_HERSHEY_SIMPLEX,
+            col: Tuple[int, int, int] = (70, 255, 50),
+            thickness = 1):
+        """text_on_im.
+
+        Parameters
+        ----------
+        im : np.ndarray
+            greyscale or BGR image
+        text : str
+            text to write on image
+        pos : Union[str, Tuple[int, int]]
+            position where text shall start
+            'tr' for top right, 'tl' for top left, else (x, y) num. tuple
+        font : str
+            do [f for f in dir(cv2) if 'FONT' in f]  to view options
+        col : Tuple[int, int, int]
+            (B, G, R) letter colour
+        thickness :
+            letter thickness
+
+        Returns
+        -------
+        np.ndarray
+            A BGR image with coloured text on it
+        """
+        size = 0.7 # fixed size to break lines properly
+        im_cpy = im.copy()
+        if len(im_cpy.shape) == 2: # to BGR for coloured text
+            im_cpy = cv2.cvtColor(im_cpy, cv2.COLOR_GRAY2BGR)
+        height, width, _ = im_cpy.shape
+        px_padding = 30
+
+        # make sure position is in coordinates
+        if pos == 'tr': # top right
+            pos = (int(width/2), px_padding)
+        elif pos == 'tl': # top left
+            pos = (px_padding, px_padding)
+       
+        l_height = 25
+        # CV doesn't support newline in putText so do it manually
+        for i, text_line in enumerate(text.split('\n')):
+            cv2.putText(im_cpy, text=text_line, org=(pos[0], pos[1]+i*l_height),
+                fontFace=font, fontScale=size, color=col, thickness=thickness)
+        return im_cpy
--- tests/test_imutils.py
+++ tests/test_imutils.py
@@ -7,6 +7,7 @@ sys.path.insert(1, os.path.join(this_script_folder, '..', 'src'))
 from imutils import ImUtils
 import numpy as np
 import time
+import cv2
 
 
 class TestImUtilsNoKeypress(unittest.TestCase):
@@ -24,7 +25,38 @@ class TestImUtilsNoKeypress(unittest.TestCase):
 
 
     def test_duration(self):
-        duration = 4 # in sec
+        duration = 3 # in sec
         time_st = time.time()
         ImUtils.qimshow(self.im_to_show, delay = duration)
-        self.assertEqual( duration - .5 < time.time() - time_st < duration + .5, True) # about 4 sec
+        # should last about `duration` seconds
+        self.assertEqual( duration - .5 < time.time() - time_st < duration + .5, True)
+
+
+class TestImUtilsText(unittest.TestCase):
+    def setUp(self):
+        self.im_grey = np.zeros((300, 500), np.uint8)
+        self.im_bgr = np.zeros((300, 500, 3), np.uint8)
+        # choose something short to test the location etc.
+        self.text = 'test'
+
+
+    def test_text(self):
+        """
+        Write some coloured text in the first quarterion of the image.
+        Check whether its centre is there and whether it's coloured.
+        """
+        im = self.im_grey
+        h, w = im.shape
+        im_text = ImUtils.text_on_im(im, self.text, pos=(int(w/2), 30))
+        im_text_grey = cv2.cvtColor(im_text, cv2.COLOR_BGR2GRAY)
+
+        # black image with (B, G, R) letters -> [0, B, G, R] list
+        colour_vals = np.unique(im_text)
+        yx_nz = np.where(im_text_grey != 0)
+        x_nz, y_nz = yx_nz[1], yx_nz[0]
+        x_centre, y_centre = np.mean(x_nz), np.mean(y_nz)
+        #ImUtils.qimshow(im_text)
+
+        self.assertEqual(x_centre > w/2 and y_centre < h/2, True)
+        # 0 + the letter colour triplet
+        self.assertEqual(len(colour_vals), 4)
