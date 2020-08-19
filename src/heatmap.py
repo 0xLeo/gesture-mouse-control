9035f2322a0769d3875565a3369e2c29d0c225a8
0xleo <0xleo.git@gmail.com>
2020-08-19 18:55:11 +0300

    add docs to Heatmap class

--- src/heatmap.py
+++ src/heatmap.py
@@ -2,35 +2,53 @@ import cv2
 import numpy as np
 from collections import deque
 import itertools
+from typing import Tuple
 
 class Heatmap():
-    def __init__(self, rows_cols: tuple):
-        self.heatmap = np.zeros((rows_cols[0], rows_cols[1]), np.uint8)
-        self.max_hits = 4
-        self._centr_window_len = 5
+    """Heatmap. A class to accumulate votes given some frames
+    and store them on a 2D plane. It finds the peak of that plane.
+    """
+    def __init__(self, size: Tuple[int, int], max_hits: int = 4, window: int = 5):
+        self.heatmap = np.zeros((size[0], size[1]), np.uint8)
+        self.max_hits = max_hits
+        self.window = window
         # stores the last detected centroids of the hand
-        self._last_centroids = deque(maxlen=self._centr_window_len+1)
+        self._last_centroids = deque(maxlen=self.window+1)
         # if the centroid moves less than that, it's considered still
-        self._min_moving_displ = 5.
+        self._min_moving_displ = 4
 
 
-    def update(self, x, y, w, h):
-        """
-        faceCascade.detectMultiScale returns (x, y, w, h)
-        Pass (x, y), (x+w, y+h) to this method
-        The region inside that rectangle adds +1 to the map
+    def update(self, x0, y0, w, h):
+        """update. Update the votes at the heat map (.heatmap).
+        Can be used in conjuction with faceCascade.detectMultiScale.
+        The latter returns a list of (x, y, w, h) tuple, each one
+        can be passed in this method.
 
+        Parameters
+        ----------
+        x0 :
+            top x0 coordinate
+        y0 :
+            top y0 coordinate
+        w :
+            width
+        h :
+            height
         """
         self.heatmap -= 1
         # fix underflow
         self.heatmap[self.heatmap == 255] = 0
-        self.heatmap[y:y+h, x:x+w] += 2
+        self.heatmap[y0:y0+h, x0:x0+w] += 2
         self.heatmap[self.heatmap > self.max_hits] = self.max_hits
 
 
-    def find_centroid(self) -> tuple:
-        """
-        get the centre of the highest area
+    def find_centroid(self) -> Tuple[int, int]:
+        """find_centroid. Find the centroid of the highest values at the map.
+
+        Returns
+        -------
+        tuple
+            The centroid (x, y)
         """
         thresh_map = np.zeros(self.heatmap.shape, np.uint8)
         thresh_map[self.heatmap == self.heatmap.max()] = 255
@@ -41,16 +59,19 @@ class Heatmap():
 
 
     def is_still(self) -> bool:
-        """
-        Looks at last n centroids and determines if object
-        is stationary
+        """is_still.
+        Looks at all the centroids and heuristically determines
+        whether the centroid trajectory is stationary.
+
+        Returns
+        -------
+        bool
+            True of stationary, else False
         """
         centrs = self._last_centroids
-        print(centrs)
         if len(centrs) > 1:
-            displs = [np.linalg.norm(r1-r2) for r1,r2 in zip(centrs,\
-                    itertools.islice(centrs,1,None))]
-            print(displs)
+            displs = [np.linalg.norm(r1-r2) for r1, r2 in
+                    zip(centrs, itertools.islice(centrs, 1, None))]
             return np.median(displs) < self._min_moving_displ
         else:
             return False
