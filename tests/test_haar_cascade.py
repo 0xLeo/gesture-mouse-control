e1785f08488f2a3a4a7f8cada4d4c0f27b07e65b
0xleo <0xleo.git@gmail.com>
2020-08-20 19:28:05 +0300

    fix some errors

--- tests/test_haar_cascade.py
+++ tests/test_haar_cascade.py
@@ -4,7 +4,7 @@ import sys
 this_script_path = os.path.abspath(__file__)
 this_script_folder = os.path.dirname(this_script_path)
 sys.path.insert(1, os.path.join(this_script_folder, '..', 'src'))
-import myimutils
+import heatmap 
 import numpy as np
 import cv2
 
@@ -14,7 +14,11 @@ class TestHaarCascade(unittest.TestCase):
         self.fpath_casc = os.path.join('..', 'cascade', 'hand.xml')
         self.fpath_vid = os.path.join('vid_detection',
                 '1_subject_1_room_10_frames.mp4')
-        if not os.path.isfile(self.fpath_casc) or not os.path.isfile(self.fpath_casc):
+        self.fpath_gt = os.path.join('vid_selection',
+                '1_subject_1_room_1_10_frames.txt')
+        if not os.path.isfile(self.fpath_casc) or\
+        not os.path.isfile(self.fpath_casc) or\
+        not os.path.isfile(self.fpath_gt):
             return # there's nothing to test
 
     
@@ -26,14 +30,14 @@ class TestHaarCascade(unittest.TestCase):
             val, frame = cap.read()
             if first:
                 r, c, _ = frame.shape
-                hm = Heatmap((r, c))
+                hm = heatmap.Heatmap((r, c))
                 first = False
             else:
                 grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
                 hands = hand_detector.detectMultiScale(grey,
                         scaleFactor = 1.2,
                         minNeighbors = 5,
-                        min_size = (64, 64))
+                        minSize = (64, 64))
                 if debug:
                     cv2.imshow("", grey)
                     cv2.waitKey(33)
