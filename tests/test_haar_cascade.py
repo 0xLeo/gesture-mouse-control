ca670ccedb7a0b29b2b4a95c858bc9b6b498a4d8
0xleo <0xleo.git@gmail.com>
2020-08-20 22:41:06 +0300

    Haar cascade test: add bounding box in debug output

--- tests/test_haar_cascade.py
+++ tests/test_haar_cascade.py
@@ -11,17 +11,18 @@ import cv2
 
 class TestHaarCascade(unittest.TestCase):
     def setUp(self):
-        self.fpath_casc = os.path.join('..', 'cascade', 'hand.xml')
-        self.fpath_vid = os.path.join('vid_detection',
+        self.fpath_casc = os.path.join(this_script_folder, '..', 'cascade', 'hand.xml')
+        self.fpath_vid = os.path.join(this_script_folder, 'vid_detection',
                 '1_subject_1_room_10_frames.mp4')
-        self.fpath_gt = os.path.join('vid_detection',
+        self.fpath_gt = os.path.join(this_script_folder, 'vid_detection',
                 '1_subject_1_room_10_frames.csv')
 
     
-    def test_haar_cascade(self, debug = True):
+    def test_haar_cascade(self, debug = False):
         if not os.path.isfile(self.fpath_casc) or\
         not os.path.isfile(self.fpath_casc) or\
         not os.path.isfile(self.fpath_gt):
+            print("[DBG] test_haar_cascade didn't find required files. Skipped.")
             return # there's nothing to test
         hand_detector = cv2.CascadeClassifier(self.fpath_casc)
         cap = cv2.VideoCapture(self.fpath_vid)
@@ -40,8 +41,10 @@ class TestHaarCascade(unittest.TestCase):
                         scaleFactor = 1.2,
                         minNeighbors = 5,
                         minSize = (64, 64))
+                x, y, w, h = hands[0][0], hands[0][1], hands[0][2], hands[0][3]
                 if debug:
-                    cv2.imshow("", grey)
+                    cv2.imshow("", cv2.rectangle(frame, (x,y), (x+w, y+h),
+                        (60, 255, 0), 3))
                     cv2.waitKey(33)
         cap.release()
         cv2.destroyAllWindows()
